ad9c2c9bf1cf6dd1f41b92dfa098af1bedd01f3c
Flower <liyw0924@gmail.com>
2019-02-23 16:09:32 +0800

    第一次提交

---
+++ src/com/test/test1.java
@@ -0,0 +1,52 @@
+package com.test;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+
+public class test1 {
+    public static void  main(String[] args){
+        String format = "HH:mm:ss";
+        SimpleDateFormat sf = new SimpleDateFormat("HH:mm:ss");
+        String now = sf.format(new Date());
+        //now = "16:59:59";
+        Date nowTime;
+        try {
+            nowTime = new SimpleDateFormat(format).parse(now);
+            Date startTime = new SimpleDateFormat(format).parse("09:00:00");
+            Date endTime = new SimpleDateFormat(format).parse("17:00:00");
+            if (isEffectiveDate(nowTime, startTime, endTime)) {
+                System.out.println("在在这 ");
+            } else {
+                System.out.println("不在不在 ");
+            }
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean isEffectiveDate(Date nowTime, Date startTime, Date endTime) {
+        if (nowTime.getTime() == startTime.getTime()
+                || nowTime.getTime() == endTime.getTime()) {
+            return true;
+        }
+
+        Calendar date = Calendar.getInstance();
+        date.setTime(nowTime);
+
+        Calendar begin = Calendar.getInstance();
+        begin.setTime(startTime);
+
+        Calendar end = Calendar.getInstance();
+        end.setTime(endTime);
+
+        if (date.after(begin) && date.before(end)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ src/com/test/test1112.java
@@ -0,0 +1,51 @@
+package com.test;
+
+import com.domin.Apple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class test1112 {
+    public static void  main(String[] args){
+        List<String> arrayOfWords = new ArrayList<String>();
+        arrayOfWords.add("Goodbye");
+        arrayOfWords.add("World");
+        List<String> uniqueCharacters =
+                arrayOfWords.stream()
+                        .map(w -> w.split(""))
+                        .flatMap(Arrays::stream)
+                        .distinct()
+                        .collect(Collectors.toList());
+        System.out.print("ok");
+    }
+    public void getNum(){
+        List<Integer> ints = Arrays.asList(1,23,5,25,88,77,14,11);
+        List<Integer> result = filter(ints,i->i>23);
+        System.out.print(result);
+        //排序
+        result.sort(Integer::compareTo);
+        System.out.print("ok");
+    }
+
+    public void getFriut(){
+        List<Apple> apples = Arrays.asList(new Apple("red",101),
+                new Apple("red",111),new Apple("yellow",103) );
+        List<Apple> biaozhun = filter(apples,(Apple a)->a.getWeight()>101&&a.getColor().equals("red"));
+        apples.sort(Comparator.comparing(Apple::getWeight));
+        System.out.print(biaozhun);
+    }
+    private static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+        List<T> result = new ArrayList<>();
+        for (T t : list) {
+            if (predicate.test(t)) {
+                result.add(t);
+            }
+        }
+        return result;
+    }
+
+}
