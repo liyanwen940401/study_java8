0c8e84bee81fee867e6198375729b55b1c8efaaf
liyw4 <liyw4@ziroom.com>
2019-02-25 18:25:16 +0800

    文件整理

--- src/com/test/test_01.java
+++ src/com/test/test_01.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,8 +33,23 @@ public class test_01 {
         System.out.println("oo");
     }
     @Test
-    public void test_num(){
+    public void test_compare(){
+        System.out.println("oo");
+        /*apples.sort(new Comparator<Apple>() {
+            //compareTo这个方法Integer才有，int没有
+            public int compare(Apple a1, Apple a2){
+                return a1.getWeight().compareTo(a2.getWeight());
+            }
+        });*/
+        apples.sort((Apple a,Apple b)->a.getWeight().compareTo(b.getWeight()));
         System.out.println("oo");
     }
+    @Test
+    public void test_runnable(){
+        System.out.println(this+"oo");
+        Thread thread = new Thread(()->System.out.println("多线程oo"));
+        System.out.println("oo");
+    }
+
 
 }
